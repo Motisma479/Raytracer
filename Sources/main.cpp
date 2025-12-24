587d69796f3c678269391a30feb4595994eb750c
Motisma479 <robion.mathieu.16@gmail.com>
2025-12-24 08:40:28 +0100

    [Feature]Add a basic test of creating a PPM image

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#include "Core/Color.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -7,5 +11,31 @@ int main(int argc, char* argv[])
     {
         std::cout << argv[i] << std::endl;
     }
+
+    int image_width = 256;
+    int image_height = 256;
+    std::string filename = "Test.PPM";
+    std::ofstream ostrm(filename, std::ios::binary);
+    ostrm << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+        
+    std::cout << std::setfill('0') << "progress:\nline: 000; row: 000";
+
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            Color col = {
+
+                double(i) / (image_width - 1),
+                0.0,
+                double(j) / (image_height - 1)
+            };
+
+            
+            ostrm << col.RGB255_str() << '\n';
+            std::cout << "\rline: " << std::setw(3) << j << "; row: " << std::setw(3) << i;
+        }
+    }
+
+    std::cout << "\nImage Finished" << std::endl;
+
     return 0;
 }
