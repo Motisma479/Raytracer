e5be0e7cbd19b279ee973ada7ddb274797708329
Motisma479 <robion.mathieu.16@gmail.com>
2026-04-23 13:56:02 +0200

    [Fix] change all function to be const

--- Headers/Core/Interval.hpp
+++ Headers/Core/Interval.hpp
@@ -9,11 +9,11 @@ public:
 	Interval();
 	Interval(f32 min_, f32 max_);
 
-	f32 Size();
-	bool Contains(f32 value_);
-	bool Surrounds(f32 value_);
+	f32 Size() const;
+	bool Contains(f32 value_) const;
+	bool Surrounds(f32 value_) const;
 
-	f32 Clamp(f32 value_);
+	f32 Clamp(f32 value_) const;
 
 	static const Interval empty, universe;
 };
--- Sources/Core/Interval.cpp
+++ Sources/Core/Interval.cpp
@@ -7,22 +7,22 @@ const Interval Interval::universe = Interval(-Maths::Constants::INF, +Maths::Con
 Interval::Interval() : min(+Maths::Constants::INF), max(-Maths::Constants::INF) {}
 Interval::Interval(f32 min_, f32 max_) : min(min_), max(max_) {}
 
-f32 Interval::Size()
+f32 Interval::Size() const
 {
 	return max - min;
 }
 
-bool Interval::Contains(f32 value_)
+bool Interval::Contains(f32 value_) const
 {
 	return min <= value_ && value_ <= max;
 }
 
-bool Interval::Surrounds(f32 value_)
+bool Interval::Surrounds(f32 value_) const
 {
 	return min < value_ && value_ < max;
 }
 
-f32 Interval::Clamp(f32 value_)
+f32 Interval::Clamp(f32 value_) const
 {
 	return (value_<min ? min : value_>max ? max : value_);
 }
